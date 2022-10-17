5577534799191494e80fb7e4c88c12ae7b5a5f3f
Veronica Rudiuk <veronicarudiuk@gmail.com>
2022-10-18 00:37:27 +0400

    Initial Commit

---
+++ Calculator Auto Layout byCode/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculator Auto Layout byCode
+//
+//  Created by Veronica Rudiuk on 18.10.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
